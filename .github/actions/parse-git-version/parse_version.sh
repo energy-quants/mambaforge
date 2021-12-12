194e0d2f73b7ea4a106b08bae874e929f4e36ea2
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2021-12-12 22:45:53 +1000

    wip

--- .github/actions/parse-git-version/parse_version.sh
+++ .github/actions/parse-git-version/parse_version.sh
@@ -1,8 +1,8 @@
-set -ox pipefail
+set -x pipefail
 
 GIT_DESCRIBE_VERSION=$(git describe --tags --long --dirty)
 
-set -eu
+set -euo
 
 if [ $? -ne 0 ]; then
   echo "Unable to locate tag, using 0.0.0"
