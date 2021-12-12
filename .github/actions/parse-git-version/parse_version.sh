14232850b666b22e074b964ccbece9609a0e5577
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2021-12-12 22:33:51 +1000

    wip

--- .github/actions/parse-git-version/parse_version.sh
+++ .github/actions/parse-git-version/parse_version.sh
@@ -1,7 +1,7 @@
 set -euox pipefail
 
 
-GIT_DESCRIBE_VERSION=$(git describe --tags --long --dirty --always)
+GIT_DESCRIBE_VERSION=$(git describe --tags --long --dirty)
 if [ $? -ne 0 ]; then
   echo "Unable to locate tag, using 0.0.0"
   MAJOR="0"
@@ -9,7 +9,7 @@ if [ $? -ne 0 ]; then
   PATCH="0"
   SHA=$(git rev-parse --short HEAD)
   POSTN=$(printf "%03d" $(git rev-list --count HEAD))
-  if [[ "${POSTN}" == "000 "]]; then
+  if [[ "${POSTN}" == "000" ]]; then
     GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}"
   else
     GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}.${POSTN}+${SHA}"
