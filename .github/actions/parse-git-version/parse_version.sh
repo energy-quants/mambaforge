73e7f945f2693776d1e72e9cecd9901e8c7a3e6f
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2021-12-13 13:27:51 +1000

    wip

--- .github/actions/parse-git-version/parse_version.sh
+++ .github/actions/parse-git-version/parse_version.sh
@@ -5,24 +5,30 @@ GIT_DESCRIBE_VERSION=$(git describe --tags --long --dirty)
 set -eu
 
 if [[ -z "${GIT_DESCRIBE_VERSION}" ]]; then
-  echo "Unable to locate tag, using 0.0.0"
-  MAJOR="0"
-  MINOR="0"
-  PATCH="0"
-  SHA=$(git rev-parse --short HEAD)
-  POSTN=$(printf "%03d" $(git rev-list --count HEAD))
-  if [[ "${POSTN}" == "000" ]]; then
-    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}"
-  else
-    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}.post${POSTN}+${SHA}"
-  fi
+    echo "Unable to locate tag, using 0.0.0"
+    MAJOR="0"
+    MINOR="0"
+    PATCH="0"
+    SHA=$(git rev-parse --short HEAD)
+    POSTN=$(printf "%03d" $(git rev-list --count HEAD))
 else
+    # split on dash: <semver>-<postn>-<sha>
     readarray -d '-' -t parts<<<"${GIT_DESCRIBE_VERSION}"
     VERSION="${parts[0]}"
     POSTN=$(printf "%03d" "${parts[1]}")
     SHA="${parts[2]%%$'\n'}"  # trim trailing newline
     SHA="${SHA#g}"  # trim leading 'g'
-    echo "XX${SHA}XX"
-    GIT_DESCRIBE_VERSION="${VERSION}.post${POSTN}+${SHA}"
+    # split on dot: <major>.<minor>.<patch>
+    readarray -d '.' -t parts<<<"${VERSION}"
+    MAJOR="${parts[0]}"
+    MINOR="${parts[1]}"
+    PATCH="${parts[2]}"
 fi
 
+if [[ "${POSTN}" == "000" ]]; then
+    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+else
+    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}.post${POSTN}+${SHA}"
+fi
+
+echo "~~~${GIT_DESCRIBE_VERSION}~~~"
