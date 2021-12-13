c44d007e8ff1133496edaeb64b31bc170a4ae618
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2021-12-13 13:34:18 +1000

    wip

--- .github/actions/parse-git-version/parse_version.sh
+++ .github/actions/parse-git-version/parse_version.sh
@@ -5,7 +5,7 @@ GIT_DESCRIBE_VERSION=$(git describe --tags --long --dirty)
 set -eu
 
 if [[ -z "${GIT_DESCRIBE_VERSION}" ]]; then
-    echo "Unable to locate tag, using 0.0.0"
+    echo "Unable to find any tags, using 0.0.0"
     MAJOR="0"
     MINOR="0"
     PATCH="0"
@@ -14,21 +14,21 @@ if [[ -z "${GIT_DESCRIBE_VERSION}" ]]; then
 else
     # split on dash: <semver>-<postn>-<sha>
     readarray -d '-' -t parts<<<"${GIT_DESCRIBE_VERSION}"
-    VERSION="${parts[0]}"
+    SEMVER="${parts[0]}"
     POSTN=$(printf "%03d" "${parts[1]}")
     SHA="${parts[2]%%$'\n'}"  # trim trailing newline
     SHA="${SHA#g}"  # trim leading 'g'
     # split on dot: <major>.<minor>.<patch>
-    readarray -d '.' -t parts<<<"${VERSION}"
+    readarray -d '.' -t parts<<<"${SEMVER}"
     MAJOR="${parts[0]}"
     MINOR="${parts[1]}"
     PATCH="${parts[2]%%$'\n'}"  # trim trailing newline
 fi
 
 if [[ "${POSTN}" == "000" ]]; then
-    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}"
+    VERSION="${MAJOR}.${MINOR}.${PATCH}"
 else
-    GIT_DESCRIBE_VERSION="${MAJOR}.${MINOR}.${PATCH}.post${POSTN}+${SHA}"
+    VERSION="${MAJOR}.${MINOR}.${PATCH}.post${POSTN}+${SHA}"
 fi
 
-echo "~~~${GIT_DESCRIBE_VERSION}~~~"
+echo "~~~${VERSION}~~~"
