2670d8d113145d0b69526332e77e982241ed4232
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2021-12-22 23:30:34 +1000

    Add debug print

--- build/images/mambaforge.sh
+++ build/images/mambaforge.sh
@@ -40,6 +40,8 @@ buildah config --env BASH_ENV=/etc/profile.d/conda.sh "${container}"
 # mamba requires a bash shell
 buildah config --entrypoint '["/bin/bash", "-lc", "$0 $@"]' "${container}"
 
+buildah run "${container}" -- echo "echo 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'" >>  /etc/profile.d/conda.sh
+
 buildah config --user 'user:user' "${container}"
 
 # image_name=`basename --suffix '.sh' "$0"`
